59de56e272afe36c5099fc1c0933c60873d1510b
blekmus <dinil@protonmail.com>
2023-03-28 11:29:42 +0530

    added site logo favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from 'next-auth/react'
 import { MantineProvider } from '@mantine/core'
 import type { MantineThemeOverride } from '@mantine/core'
 import { NotificationsProvider } from '@mantine/notifications'
 import { RouterTransition } from '../components/router_transition.component'
-import Script from 'next/script';
-
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const theme = {
@@ -32,6 +31,24 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
             defer
             data-website-id="43721fe7-dd40-4297-a31c-1192e89f4013"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/icons/site.webmanifest" />
           <Component {...pageProps} />
         </NotificationsProvider>
       </MantineProvider>
