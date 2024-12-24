9d8be23634cf3740b133216b72f4cac2f220863b
blekmus <dinil@protonmail.com>
2024-12-23 22:34:40 -0500

    security issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../lib/prisma"
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../lib/prisma'
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -13,11 +13,20 @@ export default NextAuth({
   ],
   secret: process.env.NEXTAUTH_SECRET,
   session: {
-    strategy: "database",
+    strategy: 'database',
     maxAge: 30 * 24 * 60 * 60, // 30 days
     updateAge: 2 * 60, // 2 hours,
   },
   pages: {
     signIn: '/admin',
   },
-});
+  callbacks: {
+    async signIn({ profile }) {
+      if (profile.login === 'blekmus') {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
+})
